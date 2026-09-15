bdb59f378871c387bd23f393c0581b8094c03915
victorvicente2812 <victorvicente1243@gmail.com>
2026-09-15 10:47:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,18 +9,29 @@ st.set_page_config(
 )
 
 # Carregar dados dos filmes
-
+filmes = pd.read_csv('filmes.csv')
 
 # Sidebar apenas com filtro de gênero
 # coloque a logo e a selectbox
-
+st.sidebar.title('VT-flix')
+st.sidebar.image('logo.png')
+st.sidebar.title('filtros')
+genero_escolhido = st.sidebar.selectbox(
+    "Filtrar por gênero",
+    ["Todos"] + sorted(filmes['Genre'].unique().tolist())
+)
 
 # Aplicar filtro
-
+if genero_escolhido == "todos":
+    filmes_filtrados =filmes
+else:
+    filmes_filtrados = filmes[filmes['Genre']==genero_escolhido]
 
 # Título da página
+st.title("Top filmes IMDB")
 
 # Mostrar quantidade de filmes
+st.write(f'**(len(filmes_filtrados))filmes filtrados**')
 
 
 # Mostrar filmes em grid (3 colunas)
@@ -37,6 +48,9 @@ for index, filme in filmes_filtrados.iterrows():
             st.image(filme['Image URL'], use_container_width=True)
             
             # Informações do filme
+            st.write(f'Genero: {filme['Genre']}')
+            st.write(f' Ano {filme['Year']}')
+            st.write(f'Nota {filme['Rating']}')
 
             
             # Botão para ver no IMDb
